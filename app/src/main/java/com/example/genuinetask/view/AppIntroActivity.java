e5ec751434e2c1f0ecf23f4f604b60e425ff33f2
Sajib <mashuir3@gmail.com>
2023-04-18 22:21:06 +0600

    koask

---
+++ app/src/main/java/com/example/genuinetask/view/AppIntroActivity.java
@@ -0,0 +1,16 @@
+package com.example.genuinetask.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.genuinetask.R;
+
+public class AppIntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_intro);
+    }
+}
--- app/src/main/java/com/example/genuinetask/view/SplashScreenActivity.java
+++ app/src/main/java/com/example/genuinetask/view/SplashScreenActivity.java
@@ -2,8 +2,11 @@ package com.example.genuinetask.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.example.genuinetask.MainActivity;
 import com.example.genuinetask.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -12,5 +15,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        int SPLASH_DISPLAY_LENGTH = 1200;
+
+        new Handler().postDelayed(() -> {
+
+            Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+
+
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
